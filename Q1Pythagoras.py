9881a070004af8619620d371ab4a2409dfc2e363
Anthony Bengtsson <124379293+1AnthonyB1@users.noreply.github.com>
2023-04-27 12:10:36 +0000

    Part One Finished

---
+++ Q1Pythagoras.py
@@ -0,0 +1,11 @@
+import math
+
+a = (input("What is the leg length of side a?\nenter a here:"))
+
+b = float(input("What is the leg length of side b?\nenter b here:"))
+
+csquared = ((a**2) + (b**2))
+
+result = math.sqrt(csquared)
+
+print("C equals: ",result)
